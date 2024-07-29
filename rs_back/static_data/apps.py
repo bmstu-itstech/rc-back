350d3ed849778b1be1be399f45f268cc9dccdbdc
Тимур <63254053+Timur1414@users.noreply.github.com>
2024-07-29 17:19:47 +0300

    fix: delete unnecessary models. (#2)

--- rs_back/static_data/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class StaticDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rs_back.static_data'
-    verbose_name = 'Статические данные'
